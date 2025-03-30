806823fb42756d4e2f44c6542cee6a82bf97277c
Virgil <vciurtea@gmail.com>
2025-03-30 15:49:02 +0300

    initial commit with app in place, using Spotify API, simple grid, filtered data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "@/pages/layout";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
